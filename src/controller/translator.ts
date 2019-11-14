2484054ba4ac0ad637a69be237bc2e701e0dca11
Luiz João Carvalhaes Motta <luizjoaomotta@gmail.com>
2019-11-14 00:58:14 -0300

    Add numbers dictionary

--- src/controller/translator.ts
+++ src/controller/translator.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express'
 import { onError, onSuccess } from '../utils/handler'
 
+function positive(input: string) {
+	return input
+}
+
+function negative(input: string) {
+	return input
+}
+
 export function translator(req: Request, res: Response) {
 	try {
 		const { input } = req.params
 
-		return onSuccess(res, input)
+		const integerInput = parseInt(input, 10)
+		const extenso = (integerInput > 0) ? positive(input) : negative(input)
+
+		return onSuccess(res, extenso)
 	} catch (error) {
 		return onError(res, error)
 	}
