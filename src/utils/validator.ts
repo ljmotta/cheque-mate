4f7f6c9b749bad25f8a37037732a2b5687ff3918
Luiz João Carvalhaes Motta <luizjoaomotta@gmail.com>
2019-11-16 10:56:09 -0300

    Fix validator: handle symbols and letters

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -4,13 +4,22 @@ import { invalidInputType, invalidInputValue } from './errors'
 import { onBadRequest } from './handler'
 
 /**
- * Validate the 'input' param
+ * Middleware to verify the 'input' param.
+ * Input should be a number between MIN_INPUT and MAX_INPUT,
+ * if 'input' doesn't fit into this rules will be throw a BadRequest error
+ *
  * @param req
  * @param res
  * @param next
  */
 export function validator(req: Request, res: Response, next: NextFunction) {
 	const { input } = req.params
+
+	const toValidate = (input.slice(0, 1) === '-') ? input.slice(1, input.length) : input
+	if (toValidate.match(/[$-/:-?{-~!"^_`\[\]]|[a-zA-Z]/)) {
+		return onBadRequest(res, invalidInputType(input))
+	}
+
 	const numberInput = parseInt(input, 10)
 	if (Number.isNaN(numberInput)) {
 		return onBadRequest(res, invalidInputType(input))
